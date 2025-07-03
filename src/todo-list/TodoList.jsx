609015a883958f91ed235e1ae09d28426e9ce203
fadeldnswr <fadel.achmad@ui.ac.id>
2025-07-03 20:32:31 +0700

    revise todolist using id

--- src/todo-list/TodoList.jsx
+++ src/todo-list/TodoList.jsx
@@ -2,12 +2,36 @@ import React from "react";
 import Todo from "./Todo.jsx";
 
 const TodoList = () => {
+// Create an array of todo items
+const data = [
+	{
+    id: 0,
+		text: "Learn HTML",
+		isCompleted: true,
+	},
+	{
+    id: 1,
+		text: "Learn CSS",
+		isCompleted: true,
+	},
+	{
+    id: 2,
+		text: "Learn Javascript",
+		isCompleted: true,
+	},
+	{
+    id: 3,
+		text: "Learn React",
+		isCompleted: false,
+	},
+];
+
+// Map the array to create a list of Todo components
   return (
     <ul> 
-      <Todo text="Learn HTML" isCompleted={true} isDeleted={true}/>
-      <Todo text="Learn CSS" isCompleted={true} isDeleted={true}/>
-      <Todo text="Learn Javascript" isCompleted={false} isDeleted={false}/>
-      <Todo text="Learn ReactJS" isCompleted={false} isDeleted={false}/>
+      {data.map((todo) => (
+        <Todo key={todo.id} {...todo}/>
+      ))}
     </ul>
   )
 }
